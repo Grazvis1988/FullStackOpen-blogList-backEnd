f3934ce329b831e46311110be9baa772cf3e685f
Grazvydas <grazvydas.untulis@gmail.com>
2021-05-15 13:48:06 +0200

    ex4.3 added Jest

---
+++ utils/list_helper.js
@@ -0,0 +1,9 @@
+const dummy = (blogs) => {
+	return blogs.length === 0
+		? 1
+		: 'test for SpaceX mars lander application failed. People may get lost in space :('
+}
+
+module.exports = {
+	dummy
+}
