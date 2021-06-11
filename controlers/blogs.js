c74765aa8e91aa9bf01ba4754b04c3c86436b7e4
Grazvydas <grazvydas.untulis@gmail.com>
2021-06-11 17:27:04 +0300

    ex4.20 blogList expansion step8

--- controlers/blogs.js
+++ controlers/blogs.js
@@ -1,24 +1,47 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
+const User = require('../models/users')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (req, res) => {
-	const blogs = await Blog.find({}).populate('User', { url: 1, title: 1, author: 1, _id: 1 })
+	const blogs = await Blog.find({}).populate('user', { userName: 1, name: 1, _id: 1 })
 	res.json(blogs.map(b => b.toJSON()))
 })
 
+/*const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+		return authorization.substring(7)
+	}
+	return null
+}*/
+
 blogsRouter.post('/', async (req, res) => {
 	const body = req.body
+	//	const token = getTokenFrom(req)
+	const decodedToken = jwt.verify(req.token, process.env.SECRET)
+
+	if (!( req.token && decodedToken )) {
+		return res.status(401).json({
+			error: 'Token missing or invalid'
+		})
+	}
+
+	const user = await User.findById(decodedToken.id)
 
 	if (!!body.title && !!body.url){
 		const blog = new Blog ({
 			title: body.title,
 			author: body.author,
 			url: body.url,
-			likes: body.likes ? body.likes : 0
+			likes: body.likes ? body.likes : 0,
+			user: user._id
 		})
-
 		const savedBlog = await blog.save()
-		res.json(savedBlog)
+		user.blogs = user.blogs.concat(savedBlog._id)
+		await user.save()
+
+		res.json(savedBlog.toJSON())
 	}else{
 		res.status(400).end()
 	}
--- utils/middleware.js
+++ utils/middleware.js
@@ -27,9 +27,21 @@ const errorHandler = (error, request, response, next) => {
 	next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+		request.token = authorization.substring(7)
+		next()
+	} else {
+		request.token = null
+		next()
+	}
+}
+
 module.exports = {
 	requestLogger,
 	unknownEndPoint,
-	errorHandler
+	errorHandler,
+	tokenExtractor,
 }
 
